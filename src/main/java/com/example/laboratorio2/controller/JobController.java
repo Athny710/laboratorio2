ed1699a0dbe329878b57d34e2c8a66aff9c0a646
Ivan Mata <ivan.mata@pucp.pe>
2020-04-27 20:05:27 -0500

    AgregarEditarBorrar

--- src/main/java/com/example/laboratorio2/controller/JobController.java
+++ src/main/java/com/example/laboratorio2/controller/JobController.java
@@ -1,5 +1,6 @@
 package com.example.laboratorio2.controller;
 
+import com.example.laboratorio2.Entity.Employee;
 import com.example.laboratorio2.Entity.Job;
 import com.example.laboratorio2.repository.JobRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,8 +8,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/job")
@@ -22,4 +25,32 @@ public class JobController {
         model.addAttribute("lista", lista);
         return "job/lista";
     }
+
+    @GetMapping("/edit")
+    public String editarJob(Model model,
+                            @RequestParam("id") String id) {
+
+        Optional<Job> optShipper = jobRepository.findById(id);
+
+        if (optShipper.isPresent()) {
+            Job job = optShipper.get();
+            model.addAttribute("job", job);
+            return "job/editar";
+        } else {
+            return "redirect:/job/listar";
+        }
+    }
+
+    @GetMapping("/borrar")
+    public String borrarJob(@RequestParam("id") String id){
+        Optional<Job> opt = jobRepository.findById(id);
+
+        if (opt.isPresent()) {
+            jobRepository.deleteById(id);
+            return "redirect:/job";
+        }else{
+            return "redirect:/job";
+        }
+    }
+
 }
