7a4929c62d239409453bf43174b39a66f5fa2c7c
Anthony Rojas <a20145976@pucp.pe>
2020-04-27 19:15:29 -0500

    controllers

---
+++ src/main/java/com/example/laboratorio2/controller/DepartmentController.java
@@ -0,0 +1,10 @@
+package com.example.laboratorio2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/Department")
+public class DepartmentController {
+
+}
