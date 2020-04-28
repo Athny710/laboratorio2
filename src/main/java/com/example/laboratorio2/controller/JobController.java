883617db09295ed4335a7964aeef8e7c335dfb35
Anthony Rojas <a20145976@pucp.pe>
2020-04-27 20:15:20 -0500

    vistaslistarr

--- src/main/java/com/example/laboratorio2/controller/JobController.java
+++ src/main/java/com/example/laboratorio2/controller/JobController.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -21,5 +24,12 @@ public class JobController {
         List<Job> lista = jobRepository.findAll();
         model.addAttribute("lista", lista);
         return "job/lista";
+    }
+
+    @PostMapping("/guardar")
+    public String guardar(Job job){
+        String id = job.getJob_title()+
+        jobRepository.save(job);
+        return "redirect:/job";
     }
 }
